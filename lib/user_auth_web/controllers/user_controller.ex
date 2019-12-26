aea4ad1db770b283098e737ac33f03859a3e3b15
Nirmalendu Patra <nbp@akosmic.com>
2019-12-26 11:29:14 -0600

    session code in place, not enabled

--- lib/user_auth_web/controllers/user_controller.ex
+++ lib/user_auth_web/controllers/user_controller.ex
@@ -45,10 +45,12 @@ defmodule UserAuthWeb.UserController do
     case UserAuth.AuthContext.authenticate_user(email, password) do
       {:ok, user} ->
         conn
+        |> put_session(:current_user_id, user.id)
         |> put_status(:ok)
         |> render("sign_in.json", user: user)
       {:error, message} ->
         conn
+        |> delete_session(:current_user_id)
         |> put_status(:unauthorized)
         |> render("error.json", message: message)
     end
--- lib/user_auth_web/router.ex
+++ lib/user_auth_web/router.ex
@@ -3,12 +3,32 @@ defmodule UserAuthWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :fetch_session
+  end
+
+  pipeline :authenticate_api do
+    plug :ensure_authenticated
   end
 
   scope "/api", UserAuthWeb do
     pipe_through :api
-
-    resources "/users", UserController
     post "/users/sign_in", UserController, :sign_in
+  end
+
+  scope "/api", UserAuthWeb do
+    pipe_through :api
+    resources "/users", UserController
+  end
+
+  defp ensure_authenticated(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> render("error.json", message: "User not authenticated")
+      |> halt()
+    end
   end
 end
